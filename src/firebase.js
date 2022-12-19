4db6b11ec75d7b6f8198fa924f9c5ce3424e7008
Joshua Muster <joshuamuster@gmail.com>
2022-12-18 22:46:26 -0800

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBCBtLvqqZPWQjQE-txMrkqqesUACEuglw",
+  authDomain: "joshua-muster.firebaseapp.com",
+  projectId: "joshua-muster",
+  storageBucket: "joshua-muster.appspot.com",
+  messagingSenderId: "440267435089",
+  appId: "1:440267435089:web:a4cb8edcf996cfbffa1c70",
+  measurementId: "G-MEGHRJXQ9J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
